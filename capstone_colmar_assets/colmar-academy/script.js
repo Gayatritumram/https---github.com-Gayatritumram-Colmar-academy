6c451b6a91caf34c961ca9651ff791b1729bba33
GayatriT17 <tumramgayatri27@gmail.com>
2025-08-29 13:27:40 +0530

    Done with Capstone Project
    Signed-off-by: GayatriT17 <tumramgayatri27@gmail.com>

---
+++ capstone_colmar_assets/colmar-academy/script.js
@@ -0,0 +1,7 @@
+// Simple mobile menu toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
